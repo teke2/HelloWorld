27b9766d071593b87f02ad24aa3e5113b9231e7c
g12914iy <g12914iy@s30409.tsuda.ac.jp>
2014-04-11 11:04:15 +0900

    新規でHelloWorldを作成しました。

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author g12914iy
+ */
+public class HelloWorld {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
